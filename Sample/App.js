2e683da0246416e8c11de74fe3e11f4946d9e468
MuthuFD <muthukrishnan.suresh@freshworks.com>
2022-08-23 17:12:18 +0530

    Add: Remove styling and app ids

--- Sample/App.js
+++ Sample/App.js
@@ -30,48 +30,12 @@ import {
 import {showFreshchatConversations} from './freshchat_utils'
 
 const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-        </View>
-        <View style={{marginTop: 4 , padding: 10}} >
-            <Button onPress={showFreshchatConversations} title="showConversations" color="#841584" accessibilityLabel="Learn more about this purple button"/>
-          </View>
-      </ScrollView>
+    <SafeAreaView>
+      <Button onPress={showFreshchatConversations} title="showConversations" color="#841584" accessibilityLabel="Learn more about this purple button"/>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
