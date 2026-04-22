84e206249442aba75e8d492e66e3c841efa80a5b
Harish Kumar <harish.kumar@freshworks.com>
2026-04-22 11:40:20 +0530

    Update sample App

---
+++ Sample/ios/Pods/Target Support Files/Pods-RNSample/Pods-RNSample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RNSample : NSObject
+@end
+@implementation PodsDummy_Pods_RNSample
+@end
---
+++ Sample/ios/Pods/Target Support Files/RNFreshchatSDK/RNFreshchatSDK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_RNFreshchatSDK : NSObject
+@end
+@implementation PodsDummy_RNFreshchatSDK
+@end
